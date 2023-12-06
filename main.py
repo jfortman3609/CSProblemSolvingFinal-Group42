6a72c55e7a48a2aa188cc37f7c1e96b249624ba0
James Fortman <jfortman3609@floridapoly.edu>
2023-12-06 14:55:58 -0500

    Implemented file selection
    However, this does nothing for now
    
    Will look more into it after class

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd # Necessary for selecting a file
+from tkinter.messagebox import showinfo
+
+def file_selection():
+    # Four options are listed below.
+    filetypes = (
+        ('.wav files', '*.wav'),
+        ('.mp3 files', '*.mp3'),
+        ('.aac files', '*.aac'),
+        ('All files', '*.*')
+    )
+
+    filename = fd.askopenfilename(
+        title='Choose a file...',
+        initialdir='/',
+        filetypes=filetypes)
+
+    # This seems to store the file's name and directory as a string into the variable.
+    # gfile = filename
+
+    # This opens up a prompt based on what file was selected.
+    # Should probably be scrapped for later.
+    showinfo(
+        title='Selected File',
+        message=filename
+    )
 
 root = tk.Tk()
 root.title('Replace me later')
 
-audio_button = tk.Button(root, text="Choose file...")
+audio_button = tk.Button(
+    root,
+    text="Choose file...",
+    command=file_selection
+)
 audio_button.pack()
 
 root.mainloop()
