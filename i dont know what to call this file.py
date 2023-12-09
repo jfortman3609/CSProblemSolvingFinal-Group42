e1a0139c46a2796b92872d9f2da7ba1451d9f369
James Fortman <jfortman3609@floridapoly.edu>
2023-12-08 23:25:11 -0500

    Added comments to the file

--- i dont know what to call this file.py
+++ i dont know what to call this file.py
@@ -3,13 +3,19 @@ from scipy.io import wavfile
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Uses the output.wav file in the main file directory
 dst = "output.wav"
 
+# pydub is used here to store the audio file in a variable
+# This will be used to load things like how long it lasts and such
 sound = AudioSegment.from_file(dst, format="wav")
 print(sound.duration_seconds)
 
+# scipy is used to load the file as something else other than how long it lasts
 samplerate, data = wavfile.read(dst)
 
+# Plot stuff
+# A time array is created for how long the wav file lasts
 time = np.linspace(0, sound.duration_seconds, data.shape[0])
 plt.plot(time, data)
 plt.xlabel("Time [s]")
